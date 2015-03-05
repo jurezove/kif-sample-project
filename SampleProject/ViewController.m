a7425c59700efe68eb7989dffc7661b88ee4a090
Jure Žove <jure.pase.kure@gmail.com>
2015-03-05 18:10:27 +0100

    Initial Commit

---
+++ SampleProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleProject
+//
+//  Created by Jure Zove on 05/03/15.
+//  Copyright (c) 2015 Jure Zove. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SampleProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleProject
+//
+//  Created by Jure Zove on 05/03/15.
+//  Copyright (c) 2015 Jure Zove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
